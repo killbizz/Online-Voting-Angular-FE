fff54417ca42de543a7216131d53e422fe3c4bf7
killbizz <45452791+killbizz@users.noreply.github.com>
2021-07-16 13:36:57 +0200

    Update routing.module.ts

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,3 +1,4 @@
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
@@ -25,6 +26,11 @@ const routes : Routes = [
   {
     path: 'sign-up',
     component: SignUpComponent
+  },
+  {
+    path: 'dashboard',
+    component: AdminDashboardComponent,
+    canActivate: [RouteGuardService]
   }
 ]
 
