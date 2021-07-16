727cefd6c4259513c0c738b14aa7772f0df0803a
killbizz <45452791+killbizz@users.noreply.github.com>
2021-07-16 18:17:29 +0200

    Update Election.ts

--- src/app/classes/Election.ts
+++ src/app/classes/Election.ts
@@ -2,11 +2,19 @@ export class Election {
     id: string;
     name: string;
     type: string;
+    startDate: string;
+    endDate: string;
+    parties: number[];
+    votes: number[];
 
-    constructor(id: string, name: string, type: string){
+    constructor(id: string, name: string, type: string, startDate: string, endDate: string, parties: number[], votes: number[]){
         this.id = id;
         this.name = name;
         this.type = type;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.parties = parties;
+        this.votes = votes;
     }
 
 }
